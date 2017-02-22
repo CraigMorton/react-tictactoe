4e9ce250902570aca8f8c9094132941853e58150
Craig Morton <CraigMorton22@gmail.com>
2017-02-22 11:01:39 +0000

    Refactor Board to remove pre-return logic in render

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,33 +1,25 @@
 import React from 'react'
 import Cell from './Cell.jsx'
 
-const Board = function ({
+const Board = ({
   grid,
   handleCellClick,
   winningCells,
   gameOver,
-}) {
-  let cellOnClick = handleCellClick
-  if (gameOver) cellOnClick = null
-  let winningCellIndices = winningCells
-  if (winningCells == null) {
-    winningCellIndices = []
-  }
-  return (
-    <div className='board'>
-      {grid.map((cell, index) => (
-        <Cell
-        key={index}
-        symbol={cell}
-        claimed={cell !== ''}
-        winner={winningCellIndices.includes(index)}
-        gameOver={gameOver}
-        index={index}
-        handleClick={handleCellClick}
-        />)
-      )}
-    </div>
-  )
-}
+}) => (
+  <div className='board'>
+    {grid.map((cell, index) => (
+      <Cell
+      key={index}
+      symbol={cell}
+      claimed={cell !== ''}
+      winner={winningCells.includes(index)}
+      gameOver={gameOver}
+      index={index}
+      handleClick={handleCellClick}
+      />)
+    )}
+  </div>
+)
 
 export default Board
