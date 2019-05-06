8dc666c937347b647be623646b980da6335d81b7
Craig Morton <CraigMorton22@gmail.com>
2019-05-06 16:31:22 +0100

    Add Cell tests for onClick and symbol rendering

--- src/components/__tests__/Cell.test.js
+++ src/components/__tests__/Cell.test.js
@@ -1,9 +1,63 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import Cell from '../Cell';
 
 describe('Cell component', function() {
   it('should render', function() {
     render(<Cell />);
+  });
+
+  describe('rendered data', function() {
+    it('should render X when given X', function() {
+      const { getByText } = render(<Cell symbol={'X'} />);
+      expect(() => getByText('X')).not.toThrow();
+    });
+
+    it('should render O when given O', function() {
+      const { getByText } = render(<Cell symbol={'O'} />);
+      expect(() => getByText('O')).not.toThrow();
+    });
+
+    it('should render when symbol passed is empty string', function() {
+      render(<Cell symbol={''} />);
+    });
+  });
+
+  describe('onClick behaviour', function() {
+    it('should call handleClick when clicked', function() {
+      const handleClick = jest.fn();
+      const { getByText } = render(
+        <Cell symbol={'X'} handleClick={handleClick} />,
+      );
+      fireEvent.click(getByText('X'));
+      expect(handleClick.mock.calls.length).toBe(1);
+    });
+
+    it('should disable onClick if already claimed', function() {
+      const handleClick = jest.fn();
+      const { getByText } = render(
+        <Cell symbol={'X'} claimed={true} handleClick={handleClick} />,
+      );
+      fireEvent.click(getByText('X'));
+      expect(handleClick.mock.calls.length).toBe(0);
+    });
+
+    it('should disable onClick if game is over', function() {
+      const handleClick = jest.fn();
+      const { getByText } = render(
+        <Cell symbol={'X'} gameOver={true} handleClick={handleClick} />,
+      );
+      fireEvent.click(getByText('X'));
+      expect(handleClick.mock.calls.length).toBe(0);
+    });
+
+    it('should disable onClick when part of a winning line of cells', function() {
+      const handleClick = jest.fn();
+      const { getByText } = render(
+        <Cell symbol={'X'} winner={true} handleClick={handleClick} />,
+      );
+      fireEvent.click(getByText('X'));
+      expect(handleClick.mock.calls.length).toBe(0);
+    });
   });
 });
