ce576977e3e7851b9921615e3d4812688b6381df
Craig Morton <CraigMorton22@gmail.com>
2019-06-03 15:39:16 +0100

    Remove hardcoded initial cell value & key != index

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Cell from './Cell.jsx';
+import initialState from '../state/initialState';
+const emptyCell = initialState.grid[0];
 
 export default function Board({
   grid = [],
@@ -11,9 +13,9 @@ export default function Board({
     <div className="board">
       {grid.map((cell, index) => (
         <Cell
-          key={index}
+          key={index + cell}
           symbol={cell}
-          claimed={cell !== ''}
+          claimed={cell !== emptyCell}
           winner={winningCells.includes(index)}
           gameOver={gameOver}
           index={index}
