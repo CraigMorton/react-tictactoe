4dce57d6051557f5e5a2172d1624962249281010
Craig Morton <CraigMorton22@gmail.com>
2019-06-03 15:39:16 +0100

    Remove hardcoded initial cell value & key != index

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Cell from './Cell.jsx';
 import { isGameOver, winningLine } from '../state/gameLogic';
+import initialState from '../state/initialState';
+const emptyCell = initialState.grid[0];
 
 export default function Board({ grid = [], handleCellClick }) {
   const winningCells = winningLine(grid);
@@ -8,9 +10,9 @@ export default function Board({ grid = [], handleCellClick }) {
     <div className="board">
       {grid.map((cell, index) => (
         <Cell
-          key={index}
+          key={index + cell}
           symbol={cell}
-          claimed={cell !== ''}
+          claimed={cell !== emptyCell}
           winner={winningCells.includes(index)}
           gameOver={isGameOver(grid)}
           index={index}
