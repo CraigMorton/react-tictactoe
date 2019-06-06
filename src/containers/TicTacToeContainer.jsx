6bc5c552d125914c4e7d4fe40701353307be7150
Craig Morton <CraigMorton22@gmail.com>
2019-06-06 10:31:49 +0100

    Remove opponent from state

--- src/containers/TicTacToeContainer.jsx
+++ src/containers/TicTacToeContainer.jsx
@@ -16,7 +16,6 @@ class TicTacToeContainer extends Component {
     this.setState({
       grid,
       player: getOtherPlayer(this.state.player),
-      opponent: this.state.player,
     });
   };
   restartGame = () => {
@@ -28,7 +27,7 @@ class TicTacToeContainer extends Component {
         <Board grid={this.state.grid} handleCellClick={this.handleCellClick} />
         <GameInfo
           player={this.state.player}
-          opponent={this.state.opponent}
+          opponent={getOtherPlayer(this.state.player)}
           catsGame={isCatsGame(this.state.grid)}
           gameOver={isGameOver(this.state.grid)}
         />
--- src/state/initialState.js
+++ src/state/initialState.js
@@ -1,6 +1,4 @@
 export default {
   grid: ['', '', '', '', '', '', '', '', ''],
   player: 'X',
-  opponent: 'O',
-  catsGame: false,
 };
