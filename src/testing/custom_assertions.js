99a8133f197492f3fa135d1a2bdcc9b804ed99d7
Craig Morton <CraigMorton22@gmail.com>
2019-06-11 11:11:49 +0100

    Remove unused queryBy support from expectToExist

--- src/testing/custom_assertions.js
+++ src/testing/custom_assertions.js
@@ -1,15 +1,3 @@
 export function expectToExist(query, ...queryArgs) {
-  expect(() => {
-    const result = query(...queryArgs);
-    if (result === null) {
-      throw new Error('queryBy returned null, sought element does not exist');
-    }
-    const isArray = result instanceof Array;
-    const isEmpty = result.length === 0;
-    if (isArray && isEmpty) {
-      throw new Error(
-        'queryAllBy returned an empty array, sought elements do not exist',
-      );
-    }
-  }).not.toThrow();
+  expect(() => query(...queryArgs)).not.toThrow();
 }
