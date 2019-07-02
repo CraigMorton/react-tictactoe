6a2c8f19845289b2f177f34b61d58014f25a56a1
Craig Morton <craigmorton22@gmail.com>
2019-07-02 14:34:19 +0100

    Move current player info tests into a describe block

--- src/components/__tests__/GameInfo.test.js
+++ src/components/__tests__/GameInfo.test.js
@@ -7,30 +7,36 @@ import {
 } from '../../testing/custom_assertions';
 
 describe('GameInfo component', function() {
-  describe('should display given current player when game is not over', function() {
-    it('when player is X', function() {
-      const { getByText } = render(<GameInfo gameOver={false} player={'X'} />);
-      expectToExist(getByText, 'Current player: X');
+  describe('Current player', function() {
+    describe('should display when game is not over', function() {
+      it('when player is X', function() {
+        const { getByText } = render(
+          <GameInfo gameOver={false} player={'X'} />,
+        );
+        expectToExist(getByText, 'Current player: X');
+      });
+      it('when player is O', function() {
+        const { getByText } = render(
+          <GameInfo gameOver={false} player={'O'} />,
+        );
+        expectToExist(getByText, 'Current player: O');
+      });
+      it('when player is arbitrary string', function() {
+        const { getByText } = render(
+          <GameInfo gameOver={false} player={'not a valid player symbol'} />,
+        );
+        expectToExist(getByText, 'Current player: not a valid player symbol');
+      });
     });
-    it('when player is O', function() {
-      const { getByText } = render(<GameInfo gameOver={false} player={'O'} />);
-      expectToExist(getByText, 'Current player: O');
-    });
-    it('when player is arbitrary string', function() {
-      const { getByText } = render(
-        <GameInfo gameOver={false} player={'not a valid player symbol'} />,
-      );
-      expectToExist(getByText, 'Current player: not a valid player symbol');
-    });
-  });
-  describe('should not display current player when game is over', function() {
-    it('when player is X', function() {
-      const { getByText } = render(<GameInfo gameOver={true} player={'X'} />);
-      expectNotToExist(getByText, 'Current player: X');
-    });
-    it('when player is O', function() {
-      const { getByText } = render(<GameInfo gameOver={true} player={'O'} />);
-      expectNotToExist(getByText, 'Current player: O');
+    describe('should not display when game is over', function() {
+      it('when player is X', function() {
+        const { getByText } = render(<GameInfo gameOver={true} player={'X'} />);
+        expectNotToExist(getByText, 'Current player: X');
+      });
+      it('when player is O', function() {
+        const { getByText } = render(<GameInfo gameOver={true} player={'O'} />);
+        expectNotToExist(getByText, 'Current player: O');
+      });
     });
   });
 });
