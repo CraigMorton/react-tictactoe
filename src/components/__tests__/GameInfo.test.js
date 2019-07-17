ba83d081e83da67af5b93b854a6eaa0ec2f12589
Craig Morton <craigmorton22@gmail.com>
2019-07-17 15:43:30 +0100

    Refactor GameInfo tests with inexact text querying

--- src/components/__tests__/GameInfo.test.js
+++ src/components/__tests__/GameInfo.test.js
@@ -11,6 +11,10 @@ describe('GameInfo component', function() {
   const tiedGrid = ['X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'X'];
   describe('Current player', function() {
     describe('should display when game is not over', function() {
+      it('player is not given', function() {
+        const { getByText } = render(<GameInfo grid={inProgressGrid} />);
+        expectToExist(getByText, 'Current player', { exact: false });
+      });
       it('when player is X', function() {
         const { getByText } = render(
           <GameInfo grid={inProgressGrid} player={'X'} />,
@@ -23,17 +27,12 @@ describe('GameInfo component', function() {
         );
         expectToExist(getByText, 'Current player: O');
       });
-      it('when player is arbitrary string', function() {
-        const { getByText } = render(
-          <GameInfo
-            grid={inProgressGrid}
-            player={'not a valid player symbol'}
-          />,
-        );
-        expectToExist(getByText, 'Current player: not a valid player symbol');
-      });
     });
     describe('should not display when game is over', function() {
+      it('when player is not given', function() {
+        const { getByText } = render(<GameInfo grid={tiedGrid} />);
+        expectNotToExist(getByText, 'Current player', { exact: false });
+      });
       it('when player is X', function() {
         const { getByText } = render(<GameInfo grid={tiedGrid} player={'X'} />);
         expectNotToExist(getByText, 'Current player: X');
@@ -47,14 +46,22 @@ describe('GameInfo component', function() {
   describe('Winner', function() {
     it('should display when game is won', function() {
       const xWinsGrid = ['X', 'O', '', 'X', 'O', '', 'X', '', ''];
-      const { getByText } = render(<GameInfo grid={xWinsGrid} player={'X'} />);
+      const { getByText } = render(<GameInfo grid={xWinsGrid} />);
+      expectToExist(getByText, 'Winner', { exact: false });
+    });
+    it('should display when game is won by X', function() {
+      const xWinsGrid = ['X', 'O', '', 'X', 'O', '', 'X', '', ''];
+      const { getByText } = render(<GameInfo grid={xWinsGrid} player={'O'} />);
+      expectToExist(getByText, 'Winner: X');
+    });
+    it('should display when game is won by O', function() {
+      const oWinsGrid = ['X', 'O', 'X', 'X', 'O', '', '', 'O', ''];
+      const { getByText } = render(<GameInfo grid={oWinsGrid} player={'X'} />);
       expectToExist(getByText, 'Winner: O');
     });
     it('should not display when game is in progress', function() {
-      const { getByText } = render(
-        <GameInfo grid={inProgressGrid} player={'X'} />,
-      );
-      expectNotToExist(getByText, 'Winner: O');
+      const { getByText } = render(<GameInfo grid={inProgressGrid} />);
+      expectNotToExist(getByText, 'Winner', { exact: false });
     });
   });
   describe('Tied Game', function() {
